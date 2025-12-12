cec928066c4b8434a63d941279d3e27706354149
Fajre <105254444+fajremvp@users.noreply.github.com>
2025-12-12 01:39:39 -0300

    chore: initial project structure with maven and java 21

---
+++ src/main/java/com/fajre/s4f3c0d3s/App.java
@@ -0,0 +1,13 @@
+package com.fajre.s4f3c0d3s;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/fajre/s4f3c0d3s/AppTest.java
@@ -0,0 +1,15 @@
+package com.fajre.s4f3c0d3s;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
